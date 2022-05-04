8e8b829acb9c48eea0a222803f09ac7984c99112
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-05-04 17:24:35 -0300

    Commit: Commit inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div style={{
+      alignItems:"center",
+      justifyContent:"center",
+      display:"flex",
+      height:"100vh"
+    }}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
